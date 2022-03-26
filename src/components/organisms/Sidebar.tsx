6c3a0aa9e932f07854d3f312b548b721033e8d47
novacooo <jaceknowak226@gmail.com>
2022-03-26 20:06:49 +0100

    Added better animation

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -7,32 +7,39 @@ interface WrapperProps {
   isExpanded: boolean;
 }
 
+const TRANSITION_TIME = '1s';
+
 const Wrapper = styled.nav<WrapperProps>`
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 16px;
   padding: 36px 32px;
-  width: ${({ isExpanded }) => (isExpanded ? '350px' : '150px')};
+  min-width: ${({ isExpanded }) => (isExpanded ? '350px' : '150px')};
   height: 650px;
   background-color: ${({ theme }) => theme.BACKGROUND_NAVIGATION};
   backdrop-filter: blur(30px);
   box-shadow: 0 50px 70px -30px hsla(0, 0%, 0%, 0.9);
   border: 1px solid ${({ theme }) => theme.BORDER_NAVIGATION};
   border-radius: 10px;
-  transition: width 1s;
+  transition: min-width ${TRANSITION_TIME};
 `;
 
 const PanelButtonsWrapper = styled.div`
   display: flex;
   gap: 6px;
+  width: 100%;
 `;
 
-const ExpandButtonWrapper = styled.div`
+const ExpandButtonWrapper = styled.div<WrapperProps>`
   display: flex;
   justify-content: flex-end;
+  min-width: ${({ isExpanded }) => (isExpanded ? '100%' : '40px')};
+  transition: min-width ${TRANSITION_TIME};
 `;
 
 const Divider = styled.hr`
+  width: 100%;
   border: none;
   border-top: 2px solid ${({ theme }) => theme.BORDER};
   border-radius: 10px;
@@ -57,7 +64,7 @@ const Sidebar = () => {
       {/*   <PanelButton buttonType="minimise" /> */}
       {/*   <PanelButton buttonType="maximise" /> */}
       {/* </PanelButtonsWrapper> */}
-      <ExpandButtonWrapper>
+      <ExpandButtonWrapper isExpanded={isExpanded}>
         <ExpandButton isExpanded={isExpanded} onClick={handleButtonClick} />
       </ExpandButtonWrapper>
       <Divider />
