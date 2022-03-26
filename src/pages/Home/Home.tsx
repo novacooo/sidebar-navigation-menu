6315d097afbf92163141b6ca6b3a9a300850e0bc
novacooo <jaceknowak226@gmail.com>
2022-03-26 14:50:07 +0100

    Added styled-components to project

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Header = styled.h1`
+  font-size: 32px;
+  color: antiquewhite;
+`;
+
+const Home = () => (
+  <div>
+    <Header>Home page</Header>
+  </div>
+);
+
+export default Home;
